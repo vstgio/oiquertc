27cd94b4f3a9f5c26cf8d3ae9219082bed6a999d
yurialeksndr <yuriatp@gmail.com>
2018-03-27 10:03:12 -0300

    Adiciona arquivos HTML e CSS com estruturas básicas.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var app = require("express")();
+var express = require("express")
+var app = express();
 var http = require("http").Server(app);
 
 app.get("/", function(request, response){
-  response.send("<h1>hello world</h1>");
+  response.sendFile(__dirname + "/index.html");
 });
+
+app.use(express.static(__dirname + "/public"));
 
 http.listen(3000, function(){
   console.log("working on *:3000");
