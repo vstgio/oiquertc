5f0bfbede5cc907c31ab5bd899399817ed043a4c
yurialeksndr <yuriatp@gmail.com>
2018-03-27 18:21:01 -0300

    Integração do Socket.IO ao projeto e primeiros testes para emissão de eventos.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express")
 var app = express();
 var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 app.get("/", function(request, response){
   response.sendFile(__dirname + "/index.html");
@@ -8,6 +9,18 @@ app.get("/", function(request, response){
 
 app.use(express.static(__dirname + "/public"));
 
+io.on("connection", function(socket) {
+  console.log("+ USER CONNECTED.");
+
+  socket.on("disconnect", function(){
+    console.log("- USER DISCONNECTED.")
+  });
+
+  socket.on("message", function(message){
+    console.log("> NEW MESSAGE: " + message);
+  });
+});
+
 http.listen(3000, function(){
-  console.log("working on *:3000");
+  console.log("> WORKING ON *:3000");
 });
