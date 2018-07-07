4348f86b3a4078b0332aa3d5046c46301179f064
yurialeksndr <yuriatp@gmail.com>
2018-07-07 18:50:09 -0300

    O sistema avisa quando alguém conecta ou desconecta do chat, informando a quantidade de usuárixs online.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
+var connected_users = 0;
+
 app.get("/", function(request, response){
   response.sendFile(__dirname + "/index.html");
 });
@@ -10,10 +12,17 @@ app.get("/", function(request, response){
 app.use(express.static(__dirname + "/public"));
 
 io.on("connection", function(socket) {
-  console.log("+ a new user connected.");
+  connected_users++;
+  if (connected_users == 1) {
+    io.emit("connected", "você conectou ao chat e está sozinho no momento.");
+  }
+  else {
+    io.emit("connected", "uma nova pessoa conectou. usuárixs online: " + connected_users);
+  }
 
   socket.on("disconnect", function() {
-    console.log("- a user disconnected.")
+    connected_users--;
+    io.emit("disconnected", "uma pessoa disconectou do chat. usuárixs online: " + connected_users);
   });
 
   socket.on("message", function(message) {
