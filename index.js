980f4f1de99ea0f0a036e37ce6ad14b47f13e761
yurialeksndr <yuriatp@gmail.com>
2018-07-08 22:02:34 -0300

    Novos ajustes de layout no input, organização das mensagens e background da página.

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ app.use(express.static(__dirname + "/public"));
 io.on("connection", function(socket) {
   connected_users++;
   if (connected_users == 1) {
-    io.emit("connected", "você conectou ao chat e está sozinho no momento.");
+    io.emit("connected", "[você conectou ao chat e está sozinho no momento]");
   }
   else {
-    io.emit("connected", "uma nova pessoa conectou. usuárixs online: " + connected_users);
+    io.emit("connected", "[uma nova pessoa conectou. usuárixs online: " + connected_users + "]");
   }
 
   socket.on("disconnect", function() {
     connected_users--;
-    io.emit("disconnected", "uma pessoa disconectou do chat. usuárixs online: " + connected_users);
+    io.emit("disconnected", "[uma pessoa disconectou do chat. usuárixs online: " + connected_users + "]");
   });
 
   socket.on("message", function(message) {
